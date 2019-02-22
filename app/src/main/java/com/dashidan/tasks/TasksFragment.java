3fcfb054b7f41f32027ea5b15068b2aa222cabb2
Sinbad <151229152@qq.com>
2019-02-22 18:02:53 +0800

    add webview http page

--- app/src/main/java/com/dashidan/tasks/TasksFragment.java
+++ app/src/main/java/com/dashidan/tasks/TasksFragment.java
@@ -24,8 +24,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.CheckBox;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -62,11 +61,13 @@ public class TasksFragment extends Fragment {
 
     private View mNoTasksView;
 
-    private ImageView mNoTaskIcon;
+    private WebView mWebView;
 
-    private TextView mNoTaskMainView;
-
-    private TextView mNoTaskAddView;
+//    private ImageView mNoTaskIcon;
+//
+//    private TextView mNoTaskMainView;
+//
+//    private TextView mNoTaskAddView;
 
     private LinearLayout mTasksView;
 
@@ -115,15 +116,18 @@ public class TasksFragment extends Fragment {
 
         // Set up  no tasks view
         mNoTasksView = root.findViewById(R.id.noTasks);
-        mNoTaskIcon = (ImageView) root.findViewById(R.id.noTasksIcon);
-        mNoTaskMainView = (TextView) root.findViewById(R.id.noTasksMain);
-        mNoTaskAddView = (TextView) root.findViewById(R.id.noTasksAdd);
-        mNoTaskAddView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showAddTask();
-            }
-        });
+        mWebView = root.findViewById(R.id.doc_view);
+        mWebView.loadUrl("https://dashidan.com/and_doc/python3/1.html");
+
+//        mNoTaskIcon = (ImageView) root.findViewById(R.id.noTasksIcon);
+//        mNoTaskMainView = (TextView) root.findViewById(R.id.noTasksMain);
+//        mNoTaskAddView = (TextView) root.findViewById(R.id.noTasksAdd);
+//        mNoTaskAddView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                showAddTask();
+//            }
+//        });
 
         // Set up floating action button
         FloatingActionButton fab =
@@ -282,9 +286,9 @@ public class TasksFragment extends Fragment {
         mTasksView.setVisibility(View.GONE);
         mNoTasksView.setVisibility(View.VISIBLE);
 
-        mNoTaskMainView.setText(mainText);
-        mNoTaskIcon.setImageDrawable(getResources().getDrawable(iconRes));
-        mNoTaskAddView.setVisibility(showAddView ? View.VISIBLE : View.GONE);
+//        mNoTaskMainView.setText(mainText);
+//        mNoTaskIcon.setImageDrawable(getResources().getDrawable(iconRes));
+//        mNoTaskAddView.setVisibility(showAddView ? View.VISIBLE : View.GONE);
     }
 
     public void showActiveFilterLabel() {
@@ -334,96 +338,6 @@ public class TasksFragment extends Fragment {
 
     public boolean isActive() {
         return isAdded();
-    }
-
-    private static class TasksAdapter extends BaseAdapter {
-
-        private List<Task> mTasks;
-        private TaskItemListener mItemListener;
-
-        public TasksAdapter(List<Task> tasks, TaskItemListener itemListener) {
-            setList(tasks);
-            mItemListener = itemListener;
-        }
-
-        public void replaceData(List<Task> tasks) {
-            setList(tasks);
-            notifyDataSetChanged();
-        }
-
-        private void setList(List<Task> tasks) {
-            mTasks = checkNotNull(tasks);
-        }
-
-        @Override
-        public int getCount() {
-            return mTasks.size();
-        }
-
-        @Override
-        public Task getItem(int i) {
-            return mTasks.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            View rowView = view;
-            if (rowView == null) {
-                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
-                rowView = inflater.inflate(R.layout.task_item, viewGroup, false);
-            }
-
-            final Task task = getItem(i);
-
-            TextView titleTV = (TextView) rowView.findViewById(R.id.title);
-            titleTV.setText(task.getTitleForList());
-
-            CheckBox completeCB = (CheckBox) rowView.findViewById(R.id.complete);
-
-            // Active/completed task UI
-            completeCB.setChecked(task.isCompleted());
-            if (task.isCompleted()) {
-                rowView.setBackgroundDrawable(viewGroup.getContext()
-                        .getResources().getDrawable(R.drawable.list_completed_touch_feedback));
-            } else {
-                rowView.setBackgroundDrawable(viewGroup.getContext()
-                        .getResources().getDrawable(R.drawable.touch_feedback));
-            }
-
-            completeCB.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (!task.isCompleted()) {
-                        mItemListener.onCompleteTaskClick(task);
-                    } else {
-                        mItemListener.onActivateTaskClick(task);
-                    }
-                }
-            });
-
-            rowView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    mItemListener.onTaskClick(task);
-                }
-            });
-
-            return rowView;
-        }
-    }
-
-    public interface TaskItemListener {
-
-        void onTaskClick(Task clickedTask);
-
-        void onCompleteTaskClick(Task completedTask);
-
-        void onActivateTaskClick(Task activatedTask);
     }
 
 }
