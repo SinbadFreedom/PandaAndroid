aeff46fe75cbe7e8caa38c1d6795a4b015d70dd0
SinbadFreedom <151229152@qq.com>
2019-03-05 18:56:05 +0800

    1. 修改styles.xml, 加上全屏显示，移除状态栏 2. 注释TasksActivity通过代码设置移除状态栏的代码

--- app/src/main/java/com/dashidan/tasks/TasksActivity.java
+++ app/src/main/java/com/dashidan/tasks/TasksActivity.java
@@ -51,9 +51,8 @@ public class TasksActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.tasks_act);
 
         // Set up the navigation drawer.
