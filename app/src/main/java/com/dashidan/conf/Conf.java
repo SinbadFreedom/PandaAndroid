2728e0a569fc71a0163c72405fbbc52c322c15b7
SinbadFreedom <151229152@qq.com>
2019-03-07 12:13:07 +0800

    1. 加入主页标题列表

--- app/src/main/java/com/dashidan/conf/Conf.java
+++ app/src/main/java/com/dashidan/conf/Conf.java
@@ -3,8 +3,8 @@ package com.dashidan.conf;
 public class Conf {
     public static final String LOG_TAG = "dashidan.com";
     public static final String URL_DOC_CONTENT_PRE = "https://dashidan.com/and_doc/python3/";
-    public static final String URL_CATALOG = "catalog.md";
-    public static final String URL_CATALOG_CN = "catalog.cn.md";
-    public static final String URL_HOME_PAGE_NUM = "1";
+    public static final String URL_CATALOG = "catalog.txt";
+    public static final String URL_CATALOG_CN = "catalog.cn.txt";
+    public static final String URL_HOME_PAGE_NUM = "catalog";
     public static final int TOAST_EXIT_SHOW_TIME = 2000;
 }
